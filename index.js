41b69a50a0c09603be690915bb325a15b645215b
gknanhe <cse18gknanhejd@gmail.com>
2023-04-13 14:14:48 +0530

    add view engine

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const port = 8000;
 //any request use this routes
 app.use('/',require('./routes'));
 
+//set view engine to ejs
+app.set('view engine', 'ejs');
+
+//set views to views folder
+app.set('views', './views');
+
 app.listen(port, function(err){
     if(err){
         console.log(`Error : ${err}`);
