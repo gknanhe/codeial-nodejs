311e2ca164a77ba300f0788606cea13328e289e1
Ganesh K Nanhe <74034986+gknanhe@users.noreply.github.com>
2023-09-18 22:15:01 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,15 +1,17 @@
-const mongoose = require('mongoose');
-const env = require('../config/environment');
-mongoose.connect(`mongodb://localhost/${env.db}`);
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("connection successful");
+  })
+  .catch((err) => console.log("no connection " + err));
 
 const db = mongoose.connection;
 
-db.on('error', console.error.bind('error in connecting Database'));
+// db.on('error', console.error.
+db.on("error", console.error.bind(console, "Error connecting to MongoDB"));
 
-db.once('open', function(){
-    console.log("Connected to Database:: MongoDB");
-    
-})
+db.once("open", function () {
+  console.log("Connected to Database :: MongoDB");
+});
 
 module.exports = db;
-
